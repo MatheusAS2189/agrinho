4fd697a8336af4c7714154ff85d8f32dab0e9673
MatheusAS2189 <145040983+MatheusAS2189@users.noreply.github.com>
2025-06-02 07:56:16 -0300

    .

---
+++ script.js
@@ -0,0 +1,4 @@
+// Código JavaScript
+document.getElementById('btnAcao').addEventListener('click', function() {
+    document.getElementById('mensagem').textContent = 'Cada atitude conta! Comece reciclando, economizando água e energia hoje mesmo.';
+});
